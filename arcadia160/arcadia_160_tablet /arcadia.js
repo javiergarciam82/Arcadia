5f2b4f696a9ad6c4c50130dba4cce8fcc2377a9d
javiergarciam82 <42612063+javiergarciam82@users.noreply.github.com>
2019-03-14 15:07:36 -0500

    And

---
+++ arcadia160/arcadia_160_tablet /arcadia.js
@@ -0,0 +1,94 @@
+
+$(document).ready(function(){
+
+
+    $("h1").delay("1000").fadeIn();
+
+
+    // hide #back-top first
+    $("#back-top").hide();
+
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+      
+            }
+        });
+
+
+        // scroll body to 0px on click
+        $('a#back-top').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 100);
+            return false;
+        });
+    });
+
+
+});
+
+
+// Creditos fotos
+/*$(".btn").click(function() {
+  $(".myText").toggle();  
+});*/
+
+// OR
+
+$(".credits_photo").click(function() {
+  $(".myText").toggle();  
+});
+
+$(".credits_photo").click(function() {
+  
+   var lable = $(".credits_photo").text().trim();
+
+   if(lable == "Hide") {
+     $(".credits_photo span").text("Show");
+     $(".credits_photo img").attr("src","btn_pop_more.svg");
+     $(".myText").hide();
+   }
+   else {
+     $(".credits_photo span").text("Hide");
+     $(".credits_photo img").attr("src","btn_pop_less.svg");
+     $(".myText").show();
+   }
+    
+  });
+
+
+
+  var x = document.getElementById("myAudio"); 
+/*
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+} 
+*/
+
+function toggle(el){
+    if(el.className!="pause")
+    {
+        el.src='https://www.lafabricasemana.co/javier.lafabricasemana.co/semana/arcadia/img/audio.svg';
+        el.className="pause";
+        x.pause(); 
+    }
+    else if(el.className=="pause")
+    {
+        el.src='https://www.lafabricasemana.co/javier.lafabricasemana.co/semana/arcadia/img/sin-audio.svg';
+        el.className="play";
+        x.play(); 
+    }
+    
+    return false;
+};
+
